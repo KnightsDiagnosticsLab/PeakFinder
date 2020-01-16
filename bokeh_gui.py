b67358d20293de3944faf1cb1b908c8c1092fdbf
DocSupport <43114068+DocSupport@users.noreply.github.com>
2020-01-16 08:53:03 -0800

    whatever

--- bokeh_gui.py
+++ bokeh_gui.py
@@ -15,3 +15,19 @@ text = TextInput(title="title", value='my sine wave')
 
 curdoc().add_root(row(file_input, text))
 curdoc().title = "Sliders"
+
+
+''' Outline of functions
+	get_host_file
+		callback -> convert_fsa_to_csv
+					make_dataframe
+					apply_local_southern
+					reindex_dataframe
+					plot_graph
+	get_donor_file
+		callback ->	convert_fsa_to_csv
+					make_dataframe
+					apply_local_southern
+					reindex_dataframe
+					plot_graph
+'''
