d7d29d511edcb7ccca92ea43510acd5f6a2a52cf
DocSupport <43114068+DocSupport@users.noreply.github.com>
2020-01-17 18:03:27 -0800

    Update bokeh_gui.py

--- bokeh_gui.py
+++ bokeh_gui.py
@@ -11,6 +11,11 @@ from tkinter.filedialog import askopenfilename, askdirectory, asksaveasfilename
 import pandas as pd
 from fsa import use_csv_module
 
+df = pd.DataFrame()
+data = pd.DataFrame(columns=['Sample File Name','Marker','Allele','Area'])
+results_files = []
+cases = []
+
 def select_host_callback():
 	fpath = convert_file()
 	host_text.value = basename(fpath)
@@ -35,10 +40,25 @@ def reduce_rows(df):
 	data.sort_values(by=cols, ascending=True, inplace=True)
 	return data
 
-results_files = []
-cases = []
+def select_case_callback(attrname, old, new):
+	global df
+	data = df.loc[df['Sample File Name'] == new]
+	source = ColumnDataSource(data)
+	# columns = [TableColumn(field='Sample File Name', title='Sample File Name', width=300),
+	# 			TableColumn(field='Marker', title='Marker', width=75),
+	# 			TableColumn(field='Allele', title='Allele', width=50),
+	# 			TableColumn(field='Area', title='Area', width=50),
+	# 			]
+	data_table.source.data = source.data
+
+select_case = Select(options=cases)
+select_case.on_change('value', select_case_callback)
+# data_table = DataTable(source=source, columns=columns)
+# curdoc().add_root()
 
 def select_results_callback():
+	global cases, results_files, df
+
 	root = tk.Tk()
 	root.withdraw()		# hide the root tk window
 	file_path = askopenfilename(filetypes = (('Text file', '*.txt'),
@@ -50,33 +70,27 @@ def select_results_callback():
 	root.destroy()
 	if basename(file_path) not in results_files:
 		results_files.append(basename(file_path))
-	results_text.value = '\n'.join(results_files)
+		results_text.value = '\n'.join(results_files)
 
-	df = use_csv_module(file_path)
-	df = reduce_rows(df)
-	source = ColumnDataSource(df)
-	columns = [TableColumn(field='Sample File Name', title='Sample File Name', width=300),
-				TableColumn(field='Marker', title='Marker', width=75),
-				TableColumn(field='Allele', title='Allele', width=50),
-				TableColumn(field='Area', title='Area', width=50),
-				]
-	data_table = DataTable(source=source, columns=columns)
+		df_new = use_csv_module(file_path)
+		df = pd.concat([df, df_new], axis=0, sort=True)
+		df = reduce_rows(df)
 
-	cases = sorted(list(set(cases) | set(df['Sample File Name'].to_list())))
-	# radio_button_group = RadioGroup(labels=cases)
-	select = Select(options=cases)
+		cases = sorted(list(set(df['Sample File Name'].to_list())))
+		select_case.options = cases
+		select_case.value = cases[0]
 
-	curdoc().add_root(select)
-	curdoc().add_root(column(data_table, sizing_mode='stretch_height'))
-	# curdoc().add_root(column(data_table))
-	# curdoc().add_root(data_table)
+	# data = df.loc[df['Sample File Name'] == select_case.value]
+	# source = ColumnDataSource(data)
+	# data_table.source.data = source.data
 
 # select_results = FileInput(accept='.csv, .txt, .tsv')
-# df = pd.DataFrame()
+
 select_results = Button(label='Add GeneMapper Results', button_type='success')
 select_results.on_click(select_results_callback)
 results_text = TextAreaInput(value='<results file>', disabled=True, rows=5)
 results_row = row(select_results, results_text)
+
 
 def export_template_callback():
 	root = tk.Tk()
@@ -92,7 +106,17 @@ export_template.on_click(export_template_callback)
 export_text = TextInput(value='<template file>', disabled=True)
 export_row = row(export_template, export_text)
 
-curdoc().add_root(column(results_row, host_row, donor_row, export_row))
+columns = [TableColumn(field='Sample File Name', title='Sample File Name', width=300),
+			TableColumn(field='Marker', title='Marker', width=75),
+			TableColumn(field='Allele', title='Allele', width=50),
+			TableColumn(field='Area', title='Area', width=50),
+			]
+
+source = ColumnDataSource(data)
+data_table = DataTable(columns=columns, source=source)
+
+curdoc().add_root(column(results_row, host_row, donor_row, export_row, select_case))
+curdoc().add_root(column(data_table, sizing_mode='stretch_height'))
 curdoc().title = 'PTE'
 
 
