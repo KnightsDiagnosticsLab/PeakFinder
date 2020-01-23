8566d91435a04e5cd0094ced343cfc7709bb6732
DocSupport <43114068+DocSupport@users.noreply.github.com>
2020-01-22 20:06:09 -0800

    such progress

--- bokeh_gui.py
+++ bokeh_gui.py
@@ -10,16 +10,12 @@ from tkinter import ttk
 from tkinter.filedialog import askopenfilename, askdirectory, asksaveasfilename
 
 import pandas as pd
-from fsa import use_csv_module
+from fsa import use_csv_module, fix_formatting
 import copy
 import openpyxl
+from openpyxl.styles import Alignment
 
-from extract_from_genemapper import fix_formatting
-
-
-results_files = []
-source_cases = {}
-df_cases = {}
+pd.set_option('display.max_columns', 20)
 
 
 def reduce_rows(df):
@@ -36,8 +32,7 @@ def reduce_rows(df):
 
 def on_donor_change(attrname, old, new):
 	global data_table, current_case
-	# print(old, new)
-	# print(data_table.source.selected.indices)
+
 	newest = [c for c in new if c not in old]
 	if len(newest) > 0:
 		current_case = newest[0]
@@ -46,23 +41,18 @@ def on_donor_change(attrname, old, new):
 
 
 def on_host_click(attrname, old, new):
-	# print(new)
 	global source_cases, data_table, current_case
+
 	current_case = new
-	# indices = df.index[df['Sample File Name'] == new].tolist()
-	# view = CDSView(source=source, filters=[IndexFilter(indices)])
-	# data_table.view = view
-	# data_table.source.selected.indices = sorted(list(universal_selected))
 	data_table.source.data = source_cases[new].data
 	data_table.source.selected.indices = source_cases[new].selected.indices[:]
-	# curdoc().add_root(tables[new])
-
 
 
 def on_results_click():
-	global source_cases, results_files, df, source, data_table
+	global source_cases, results_files, df, source, data_table, df_cases
 
 	root = tk.Tk()
+	root.attributes("-topmost", True)
 	root.withdraw()		# hide the root tk window
 	file_path = askopenfilename(filetypes = (('Text file', '*.txt'),
 											('Comma Separated Values','*.csv'),
@@ -79,10 +69,6 @@ def on_results_click():
 		df_new = reduce_rows(df_new)
 
 		case_names = sorted(list(set(df_new['Sample File Name'].to_list())))
-		# if len(case_names) > 10:
-		# 	print(case_names)
-		# 	select_donor_case.size = len(case_names)
-		# 	print('select_donor_case.size = {}'.format(select_donor_case.size))
 
 		for case in case_names:
 			df_copy = df_new.loc[df_new['Sample File Name'] == case].copy(deep=True)
@@ -96,7 +82,8 @@ def on_results_click():
 			source_cases[case].selected.indices = indices
 
 		select_host_case.options = list(source_cases.keys())
-		select_donor_case.options = list(source_cases.keys())
+		select_donor_cases.options = list(source_cases.keys())
+		select_samples.options = list(source_cases.keys())
 
 	if len(results_files) == 1:
 		select_host_case.value = list(source_cases.keys())[0]
@@ -117,21 +104,31 @@ def pre_selected_indices(df):
 	return indices
 
 
-def string_to_number(s):
-	try:
-		s = float(s)
-	except:
-		pass
-	return s
+def make_template_xlsx(file_path):
+
+	''' Helper function '''
+	def string_to_number(s):
+		try:
+			s = float(s)
+		except:
+			pass
+		return s
+
+	''' Helper function '''
+	def format_value(cell_value):
+		if cell_value is not None:
+			cell_value_formatted = str(cell_value).replace('.0', '')
+		else:
+			cell_value_formatted = None
+		return cell_value_formatted
 
 
-def save_excel_file(file_path):
 	markers = ['D8S1179', 'D21S11', 'D7S820', 'CSF1PO', 'D3S1358', 'TH01', 'D13S317', 'D16S539', 'D2S1338', 'D19S433', 'vWA', 'TPOX', 'D18S51', 'AMEL', 'D5S818', 'FGA']
 	host_case = select_host_case.value
 	df_host = df_cases[host_case]
 	df_host = df_host.loc[df_host['Selected'] == True]
 
-	donor_cases = select_donor_case.value
+	donor_cases = select_donor_cases.value
 	donors = {}
 	for donor_case in donor_cases:
 		df_donor = df_cases[donor_case]
@@ -201,64 +198,305 @@ def save_excel_file(file_path):
 	ws.cell(row=2, column=2*caa, value='Formula')
 	ws.cell(row=ws.max_row+1, column=2*caa-2, value='%Host')
 	ws.cell(row=ws.max_row+1, column=2*caa-2, value='%Donor')
+
+	'''	Add the actual formulae. For now only if there's one donor, because
+		I haven't been given a cheatsheet for 2+ donors.
+	'''
+	percent_host_col = 2*caa - 1		# col num where formula goes. Actual Excel formula, not the text version.
+	d1_col = percent_host_col - 4		# col num for donor allele 1
+	d2_col = percent_host_col - 3		# col num for donor allele 2
+	h1_col = percent_host_col - 2		# col num for host allele 1
+	h2_col = percent_host_col - 1		# col num for host allele 2
+	for r2 in range(4, 4+2*len(markers), 2):
+		r1 = r2 - 1
+
+		d1_alle = ws.cell(row=r1, column=d1_col)
+		d2_alle = ws.cell(row=r1, column=d2_col)
+		h1_alle = ws.cell(row=r1, column=h1_col)
+		h2_alle = ws.cell(row=r1, column=h2_col)
+
+		d1_alle_val = format_value(d1_alle.value)
+		d2_alle_val = format_value(d2_alle.value)
+		h1_alle_val = format_value(h1_alle.value)
+		h2_alle_val = format_value(h2_alle.value)
+
+		d1_area = ws.cell(row=r2, column=d1_col)
+		d2_area = ws.cell(row=r2, column=d2_col)
+		h1_area = ws.cell(row=r2, column=h1_col)
+		h2_area = ws.cell(row=r2, column=h2_col)
+
+		f1 = ws.cell(row=r1, column=percent_host_col+1)
+		f1.alignment = Alignment(horizontal='center')
+		f2 = ws.cell(row=r2, column=percent_host_col+1)
+		f2.alignment = Alignment(horizontal='left')
+
+		d = {d1_alle_val, d2_alle_val}
+		d.discard(None)
+		h = {h1_alle_val, h2_alle_val}
+		h.discard(None)
+
+		percent_host = ws.cell(row=r2, column=percent_host_col)
+
+		# print('d = {}, h = {}'.format(d,h))
+
+		if len(d | h) == 4:
+			f1.value = '{} + {}'.format(h1_alle_val,
+										h2_alle_val)
+			f1.alignment = Alignment(horizontal='center')
+
+			f2.value = '{} + {} + {} + {}'.format(h1_alle_val,
+													h2_alle_val,
+													d1_alle_val,
+													d2_alle_val)
+
+			percent_host.value = '=100*SUM({}:{})/SUM({}:{})'.format(h1_area.coordinate,
+																h2_area.coordinate,
+																d1_area.coordinate,
+																h2_area.coordinate)
+
+		if len(h) == 1 and len(h | d) == 3:
+			f1.value = '{}'.format(h1_alle_val)
+			f1.alignment = Alignment(horizontal='center')
+
+			f2.value = '{} + {} + {}'.format(h1_alle_val,
+											d1_alle_val,
+											d2_alle_val)
+
+			percent_host.value = '=100*{}/SUM({}:{})'.format(h1_area.coordinate,
+														d1_area.coordinate,
+														h1_area.coordinate)
+
+		if len(d) == 1 and len( h | d) == 3:
+			f1.value = '{} + {}'.format(h1_alle_val,
+										h2_alle_val)
+			f1.alignment = Alignment(horizontal='center')
+
+			f2.value = '{} + {} + {}'.format(h1_alle_val,
+											h2_alle_val,
+											d1_alle_val)
+
+			percent_host.value = '=100*SUM({}:{})/SUM({},{}:{})'.format(h1_area.coordinate,
+																h2_area.coordinate,
+																d1_area.coordinate,
+																h1_area.coordinate,
+																h2_area.coordinate)
+
+		if len(h) == 1 and len(d) == 1 and len(h | d) == 2:
+			f1.value = '{}'.format(h1_alle_val)
+			f1.alignment = Alignment(horizontal='center')
+
+			f2.value = '{} + {}'.format(h1_alle_val,
+										d1_alle_val)
+			f2.alignment = Alignment(horizontal='center')
+
+			percent_host = '={}/SUM({},{})'.format(h1_area.coordinate,
+												d1_area.coordinate,
+												h1_area.coordinate)
+
+		if len(h) == 2 and len(d) == 2 and len(h | d) == 3:
+			if h1_alle_val not in d:
+				h_unique_alle_val = h1_alle_val
+				h_unique_area = h1_area
+			else:
+				h_unique_alle_val = h2_alle_val
+				h_unique_area = h2_area
+
+			if d1_alle_val not in h:
+				d_unique_alle_val = d1_alle_val
+				d_unique_area = d1_area
+			else:
+				d_unique_alle_val = d2_alle_val
+				d_unique_area = d2_area
+
+			f1.value = '{}'.format(h_unique_alle_val)
+			f1.alignment = Alignment(horizontal='center')
+
+			f2.value = '{} + {}'.format(h_unique_alle_val,
+										d_unique_alle_val)
+			f2.alignment = Alignment(horizontal='center')
+
+			percent_host.value = '=100*{}/SUM({},{})'.format(h_unique_area.coordinate,
+												d_unique_area.coordinate,
+												h_unique_area.coordinate)
+
+		if len(h) == 1 and len(d) == 2 and len(h | d) == 2:
+			if d1_alle_val in h:
+				A_alle_val = d2_alle_val
+				A_area = d2_area
+			else:
+				A_alle_val = d1_alle_val
+				A_area = d1_area
+
+			if h1_alle_val in d:
+				H_alle_val = h1_alle_val
+				H_area = h1_area
+			else:
+				H_alle_val = h2_alle_val
+				H_area = h2_area
+
+			f2.value = '(1-(2*{}/({}+{})))'.format(A_alle_val,
+													A_alle_val,
+													H_alle_val)
+
+			percent_host.value = '=100*(1-(2*{}/({}+{})))'.format(A_area.coordinate,
+																A_area.coordinate,
+																H_area.coordinate)
+		
+		if len(h) == 2 and len(d) == 1 and len(h | d) == 2:
+			if h1_alle_val in d:
+				A_alle_val = h2_alle_val
+				A_area = h2_area
+			else:
+				A_alle_val = h1_alle_val
+				A_area = h1_area
+
+			if d1_alle_val in h:
+				D_alle_val = d1_alle_val
+				D_area = d1_area
+			else:
+				D_alle_val = d2_alle_val
+				D_area = d2_area
+
+			f1.value = '2 * {}'.format(A_alle_val)
+			f1.alignment = Alignment(horizontal='center')
+
+			f2.value = '{} + {}'.format(A_alle_val,
+										D_alle_val)
+			f2.alignment = Alignment(horizontal='center')
+
+			percent_host.value = '=100*(2*{})/({}+{})'.format(A_area.coordinate,
+																A_area.coordinate,
+																D_area.coordinate)
+
+	'''	Formula for average of Percent Host column
+	'''
+	percent_host_avg = ws.cell(row=3+2*len(markers), column=percent_host_col)
+	start = ws.cell(row=3, column=percent_host_col)
+	end = ws.cell(row=2+2*len(markers), column=percent_host_col)
+	percent_host_avg.value = '=AVERAGE({}:{})'.format(start.coordinate, end.coordinate)
+
+	percent_donor_avg = ws.cell(row=4+2*len(markers), column=percent_host_col)
+	percent_donor_avg.value = '=100-{}'.format(percent_host_avg.coordinate)
+
+	'''	Save the file before running fix_formatting
+	'''
 	wb.save(file_path)
+
+	'''	Fix formatting
+	'''
 	fix_formatting(file_path)
-	# wb.save(file_path)
 
 
 def on_export_template_click():
 	root = tk.Tk()
+	root.attributes("-topmost", True)
 	root.withdraw()		# hide the root tk window
 	file_path = asksaveasfilename(defaultextension = '.xlsx',
 									filetypes=[('Excel', '*.xlsx')],
 									title = 'Save Template')
 	root.destroy()
-	export_text.value = basename(file_path)
-	save_excel_file(file_path)
+	# print('******** file_path = {} '.format(file_path))
+	if file_path.endswith('.xlsx'):
+		make_template_xlsx(file_path)
 
 
 def on_selected_change(attrname, old, new):
-	global current_case
+	global current_case, df_cases
 
 	indices = data_table.source.selected.indices[:]
 	source_cases[current_case].selected.indices = indices[:]
 	df_cases[current_case].loc[:,'Selected'] = False
 	df_cases[current_case].loc[indices,'Selected'] = True
-	# print(df_cases[current_case])
+
+
+def on_select_template_click():
+	root = tk.Tk()
+	root.attributes("-topmost", True)
+	root.withdraw()		# hide the root tk window
+	file_path = askopenfilename(filetypes=[('Excel', '*.xlsx')],
+								title = 'Choose Template')
+	root.destroy()
+
+	''' Update template_text box'''
+	template_text.value = basename(file_path)
+	global template_path
+	template_path = file_path
+	df = pd.read_excel(template_path)
+	df.loc[-2] = ''
+	df.loc[-1] = df.columns.tolist()
+	df.index = df.index + 2
+	df.sort_index(inplace=True)
+	col_letters = [openpyxl.utils.get_column_letter(i+1) for i in range(0,len(df.columns.tolist()))]
+	df.columns = col_letters
+	df = df.fillna('')
+	columns = [TableColumn(field=col, title=col) for col in df.columns.tolist()]
+	template_table.columns = columns
+	template_table.source.data = ColumnDataSource(df).data
+
+
+def populate_template_xlsx(file_path):
+	# assert file_path.endswith('.xlsx')
+	# wb = openpyxl.load_workbook(file_path)
+	# ws = wb.worksheets[0]
+	pass
+
+
+def on_populate_results_click():
+	for sample in select_samples.value:
+		root = tk.Tk()
+		root.attributes("-topmost", True)
+		root.withdraw()		# hide the root tk window
+		file_path = asksaveasfilename(defaultextension='.xlsx',
+										filetypes=[('Excel', '*.xlsx')],
+										title='Populate results for {}'.format(sample))
+		root.destroy()
+
+		populate_template_xlsx(file_path)
+
+results_files = []
+source_cases = {}
+df_cases = {}
 
 select_results = Button(label='Add GeneMapper Results', button_type='success')
 select_results.on_click(on_results_click)
 results_text = TextAreaInput(value='<results file>', disabled=True, rows=5)
 
 
-select_host_case = Select(title='Select Host', options=list(source_cases.keys()))
+# select_host_case = Select(title='Select Host', options=list(source_cases.keys()))
+select_host_case = Select(title='Select Host', options=[])
 select_host_case.on_change('value', on_host_click)
 
 
-select_donor_case = MultiSelect(title='Select Donor(s) <ctrl+click to multiselect>',
-								options=list(source_cases.keys()), size=20)
-select_donor_case.on_change('value', on_donor_change)
+select_donor_cases = MultiSelect(title='Select Donor(s) <ctrl+click to multiselect>',
+								options=[],
+								size=20)
+select_donor_cases.on_change('value', on_donor_change)
 
 
 export_template = Button(label='Export Template', button_type='warning')
 export_template.on_click(on_export_template_click)
-export_text = TextInput(value='<template file>', disabled=True)
 
 
 enter_host_name = TextInput(title='Enter Host Name', value='<type host name here>')
 
-columns = [
-			TableColumn(field='Sample File Name', title='Sample File Name', width=300),
+
+select_template = Button(label='Select Template', button_type='success')
+select_template.on_click(on_select_template_click)
+template_text = TextAreaInput(value='<template file>', disabled=True)
+
+
+select_samples = MultiSelect(title='Select Sample(s) <ctrl+click to multiselect>',
+									options=[],
+									size=20)
+
+
+populate_results = Button(label='Populate Results', button_type='warning')
+populate_results.on_click(on_populate_results_click)
+
+columns = [TableColumn(field='Sample File Name', title='Sample File Name', width=300),
 			TableColumn(field='Marker', title='Marker', width=75),
 			TableColumn(field='Allele', title='Allele', width=50),
-			TableColumn(field='Area', title='Area', width=50),
-			]
-
-col_temp = [
-			TableColumn(field='Marker', title='Marker', width=75),
-			TableColumn(field='Allele_1', title='Allele_1', width=50),
-			TableColumn(field='Allele_2', title='Allele_2', width=50),
-			]
+			TableColumn(field='Area', title='Area', width=50)]
 
 
 source = ColumnDataSource()
@@ -266,10 +504,31 @@ source.selected.on_change('indices', on_selected_change)
 data_table = DataTable(columns=columns, source=source, selectable='checkbox')
 template_table = DataTable(source=ColumnDataSource())
 
-col_0 = column(enter_host_name, select_results, results_text, select_host_case, select_donor_case, export_template)
+
+col_0 = column(enter_host_name,
+				select_results,
+				results_text,
+				select_host_case,
+				select_donor_cases,
+				export_template)
+
 col_1 = column(data_table, sizing_mode='stretch_height')
 
-curdoc().add_root(row(col_0, col_1, sizing_mode='stretch_height'))
+col_2 = column(select_results,
+				results_text,
+				select_template,
+				template_text,
+				select_samples,
+				populate_results)
+
+col_3 = column(template_table, sizing_mode='stretch_both')
+
+child_0 = row(col_0, col_1, sizing_mode='stretch_height')
+child_1 = row(col_2, col_3, sizing_mode='stretch_height')
+tab1 = Panel(child=child_0, title='Make Template')
+tab2 = Panel(child=child_1, title='Populate Results')
+tabs = Tabs(tabs=[tab1, tab2])
+curdoc().add_root(tabs)
 curdoc().title = 'PTE'
 
 
--- extract_from_genemapper.py
+++ extract_from_genemapper.py
@@ -10,7 +10,7 @@ from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Align
 import string
 import win32com.client as win32
 import csv
-from fsa import use_csv_module
+from fsa import use_csv_module, fix_formatting
 
 pd.set_option('display.max_columns', 20)
 
@@ -201,7 +201,8 @@ def insert_formulae(filename, template):
         formula_dict[cell1.value] = cell2.value
     formula_dict.pop(None)
 
-    # open outputfile and insert formulae
+    ''' open outputfile and insert formulae
+    '''
     wb = openpyxl.load_workbook(filename)
     ws = wb.worksheets[0]
 
@@ -217,103 +218,9 @@ def insert_formulae(filename, template):
     wb.save(filename)
 
 
-def border_add(border, top=None, right=None, left=None, bottom=None):
-    if top is None:
-        top = border.top
-    if left is None:
-        left = border.left
-    if right is None:
-        right = border.right
-    if bottom is None:
-        bottom = border.bottom
-    return openpyxl.styles.Border(
-        top=top, left=left, right=right, bottom=bottom)
 
 
-def fix_formatting(filename, header=None, case_name=None):
-    print(filename)
-    assert filename.endswith('.xlsx')
-    thin = Side(border_style='thin')
-    medium = Side(border_style='medium')
-    df = pd.read_excel(filename)
-    wb = openpyxl.load_workbook(filename)
-    ws = wb.worksheets[0]
-    # row_count = ws.max_row
-    # column_count = ws.max_column
 
-    # insert header
-    # ws.oddHeader = header
-
-    # by default make all cells horizontal='center
-    cells = [ws[c + str(r)] for c in string.ascii_uppercase[0:ws.max_column]
-             for r in range(1, ws.max_row + 1)]
-    for cell in cells:
-        cell.alignment = Alignment(horizontal='center')
-
-    # make first column bold and right aligned
-    cells = [ws['A' + str(r)] for r in range(1, ws.max_row - 1)]
-    for cell in cells:
-        # cell.font = Font(bold=True)
-        cell.alignment = Alignment(horizontal='right')
-        cell.border = border_add(cell.border, right=medium)
-
-    # Make first two rows bold and left aligned
-    cells = [ws[c + str(i)] for i in range(1, 3)
-             for c in string.ascii_uppercase[0:ws.max_column]]
-    for cell in cells:
-        cell.font = Font(bold=True)
-        cell.alignment = Alignment(horizontal='left')
-    # get locations of 'Allele'
-    allele_ij = []
-    for i in df.index:
-        for j, v in enumerate(df.iloc[i]):
-            if v == 'Allele':
-                allele_ij.append([i, j])
-
-    # apply medium thickness based on which cells have the word 'Allele'
-    for i, j in allele_ij:
-        for k in range(0, ws.max_column):
-            cell = ws[string.ascii_uppercase[k] + str(i + 3)]
-            cell.border = border_add(cell.border, bottom=medium)
-
-        for k in range(0, j, 2):
-            cell = ws[string.ascii_uppercase[k] + str(i + 2)]
-            cell.border = border_add(cell.border, right=medium)
-
-            cell = ws[string.ascii_uppercase[k] + str(i + 3)]
-            cell.border = border_add(cell.border, right=medium)
-
-            cell = ws[string.ascii_uppercase[k + j] + str(i + 2)]
-            cell.border = border_add(cell.border, right=medium)
-
-            cell = ws[string.ascii_uppercase[k + j] + str(i + 3)]
-            cell.border = border_add(cell.border, right=medium)
-
-        cell = ws[string.ascii_uppercase[2 * j] + str(i + 2)]
-        cell.border = border_add(cell.border, right=medium)
-
-        cell = ws[string.ascii_uppercase[2 * j] + str(i + 3)]
-        cell.border = border_add(cell.border, right=medium)
-
-    # add in case_name
-    if case_name is not None:
-        loc = location_of_value(ws, 'Post-T:')
-        # c =
-        cell = ws[chr(ord(loc[0]) + 1) + str(loc[1])]
-        cell.value = case_name
-
-    if header is not None:
-        ws.oddHeader = header
-    ws.sheet_view.view = 'pageLayout'
-    openpyxl.worksheet.worksheet.Worksheet.set_printer_settings(
-        ws, paper_size=1, orientation='landscape')
-    ws.page_margins.bottom = 0.5
-    ws.page_margins.top = 0.5
-    ws.page_margins.left = 0.5
-    ws.page_margins.right = 0.5
-    ws.page_margins.header = 0.1
-
-    wb.save(filename)
 
 
 def main():
